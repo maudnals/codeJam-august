bed6182fa827ba275a43aec5ab12f739e27a48bc
Maud Nalpas <maud.nalpas@gmail.com>
2017-08-19 17:29:58 +0200

    1st commit

---
+++ scripts/main.js
@@ -0,0 +1,188 @@
+/*
+*
+*  Push Notifications codelab
+*  Copyright 2015 Google Inc. All rights reserved.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License
+*
+*/
+
+/* eslint-env browser, es6 */
+
+// 'use strict';
+
+// const applicationServerPublicKey = 'BCZQJbnuw1B1vhHZLf4RmeqTShNkPopC6sDAWxIQb3_DuIQyuTNTYYpobHMDW_KyYrP1zmja-M1XCD9NhRyablk';
+
+// const pushButton = document.querySelector('.js-push-btn');
+
+// let isSubscribed = false;
+// let swRegistration = null;
+
+// function urlB64ToUint8Array(base64String) {
+//   const padding = '='.repeat((4 - base64String.length % 4) % 4);
+//   const base64 = (base64String + padding)
+//     .replace(/\-/g, '+')
+//     .replace(/_/g, '/');
+
+//   const rawData = window.atob(base64);
+//   const outputArray = new Uint8Array(rawData.length);
+
+//   for (let i = 0; i < rawData.length; ++i) {
+//     outputArray[i] = rawData.charCodeAt(i);
+//   }
+//   return outputArray;
+// }
+
+// if ('serviceWorker' in navigator && 'PushManager' in window) {
+//   console.log('Service Worker and Push is supported');
+
+//   navigator.serviceWorker.register('sw.js')
+//   .then(function(swReg) {
+//     console.log('Service Worker is registered', swReg);
+
+//     swRegistration = swReg;
+//   })
+//   .catch(function(error) {
+//     console.error('Service Worker Error', error);
+//   });
+// } else {
+//   console.warn('Push messaging is not supported');
+//   pushButton.textContent = 'Push Not Supported';
+// }
+
+
+// function initialiseUI() {
+//   pushButton.addEventListener('click', function() {
+//     pushButton.disabled = true;
+//     if (isSubscribed) {
+//       // TODO: Unsubscribe user
+//     } else {
+//       subscribeUser();
+//     }
+//   });
+
+//   // Set the initial subscription value
+//   swRegistration.pushManager.getSubscription()
+//   .then(function(subscription) {
+//     isSubscribed = !(subscription === null);
+
+//     updateSubscriptionOnServer(subscription);
+
+//     if (isSubscribed) {
+//       console.log('User IS subscribed.');
+//     } else {
+//       console.log('User is NOT subscribed.');
+//     }
+
+//     updateBtn();
+//   });
+// }
+
+// function updateBtn() {
+//   if (Notification.permission === 'denied') {
+//     pushButton.textContent = 'Push Messaging Blocked.';
+//     pushButton.disabled = true;
+//     updateSubscriptionOnServer(null);
+//     return;
+//   }
+
+//   if (isSubscribed) {
+//     pushButton.textContent = 'Disable Push Messaging';
+//   } else {
+//     pushButton.textContent = 'Enable Push Messaging';
+//   }
+
+//   pushButton.disabled = false;
+// }
+
+// function subscribeUser() {
+//   const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+//   swRegistration.pushManager.subscribe({
+//     userVisibleOnly: true,
+//     applicationServerKey: applicationServerKey
+//   })
+//   .then(function(subscription) {
+//     console.log('User is subscribed.');
+
+//     updateSubscriptionOnServer(subscription);
+
+//     isSubscribed = true;
+
+//     updateBtn();
+//   })
+//   .catch(function(err) {
+//     console.log('Failed to subscribe the user: ', err);
+//     updateBtn();
+//   });
+// }
+
+// function updateSubscriptionOnServer(subscription) {
+//   // TODO: Send subscription to application server
+
+//   const subscriptionJson = document.querySelector('.js-subscription-json');
+//   const subscriptionDetails =
+//     document.querySelector('.js-subscription-details');
+
+//   if (subscription) {
+//     subscriptionJson.textContent = JSON.stringify(subscription);
+//     subscriptionDetails.classList.remove('is-invisible');
+//   } else {
+//     subscriptionDetails.classList.add('is-invisible');
+//   }
+// }
+
+// navigator.serviceWorker.register('sw.js')
+// .then(function(swReg) {
+//   console.log('Service Worker is registered', swReg);
+
+//   swRegistration = swReg;
+//   initialiseUI();
+// })
+
+
+
+// function displayNotification() {
+//   if (Notification.permission == 'granted') {
+//     navigator.serviceWorker.getRegistration().then(function(reg) {
+//     const options = {
+//         body: 'You really do',
+//         image: 'images/merkel.jpg',
+//         icon: 'images/merkel.jpg',
+//         badge: 'images/merkel.jpg',
+//         vibrate: [200, 100, 200, 100, 200, 100, 200]
+//     };
+//       reg.showNotification('🖕 WOW!!! YOU SUCK!!! 🖕', options);
+//     });
+//   }
+// }
+
+// Notification.requestPermission(function(status) {
+//     console.log('Notification permission status:', status);
+// });
+
+// function notify() {
+  
+//   setInterval(function(){
+//   displayNotification();
+// }, 1200);
+// }
+
+
+navigator.serviceWorker.register('sw.js');
+Notification.requestPermission(function(result) {
+  if (result === 'granted') {
+    navigator.serviceWorker.ready.then(function(registration) {
+      registration.showNotification('Notification with ServiceWorker');
+    });
+  }
+})
